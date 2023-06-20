2ee3524185a1cdf60620e45f5df44069c7da3e89
Chirran <swainchittaranjan345@gmail.com>
2023-06-20 13:18:15 +0530

    updated main layout

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -31,8 +31,8 @@ const MainLayout = () => {
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo">
           <h2 className="text-white fs-5 text-center py-3 mb-0">
-            <span className="sm-logo">DC</span>
-            <span className="lg-logo">Dev Corner</span>
+            <span className="sm-logo">CRS</span>
+            <span className="lg-logo">Clicky Corp</span>
           </h2>
         </div>
         <Menu
